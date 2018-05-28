b6263040d40a5a00ae99e3e566a7ccf483f1a966
endorfina <emilke.plox@outlook.com>
2018-05-28 12:13:31 +0200

    faster insertion for colonies

--- src/echo/colony.hpp
+++ src/echo/colony.hpp
@@ -18,50 +18,41 @@
 */
 
 #pragma once
-#include <utility> // pair
+#include <utility>  // pair
 #include <memory>
 #include <vector>
-#include <exception>
+#include <stack>    // perhaps change later to <priority_queue>, as of now
+                    // the lagging skip points aren't being invalidated
 
 namespace Violet
 {
-    template<typename A, unsigned int ChunkSize = 64,
+    template<typename A, unsigned int ChunkSize = 256,
             typename = std::enable_if_t<std::is_trivially_destructible_v<A> && ChunkSize >= 8>>
     class colony {
     public:
         using value_type = A;
 
     private:
-        using pair_t = std::pair<unsigned int, value_type>;
-        using list_t = std::vector<std::unique_ptr<pair_t[]>>;
+        using pos_t =           unsigned int;
+        using pair_t =          std::pair<pos_t, value_type>;
+        using list_t =          std::vector<std::unique_ptr<pair_t[]>>;
+        using stack_pair_t =    std::pair<pair_t*, pos_t>;
+        using stack_t = std::stack<stack_pair_t>;
 
         list_t _data;
+        stack_t _stack;
         size_t _inner_size = 0;
 
     public:
         class iterator {
-            unsigned int _chunk_pos = 0;
+            pos_t _chunk_pos = 0;
             typename list_t::iterator _l_it;
             size_t _limit;
 
-            void _iterate() {
-                ++_chunk_pos;
-                while (true) {
-                    if (_chunk_pos >= ChunkSize) {
-                        _chunk_pos = 0;
-                        continue;
-                    }
-                    if (const auto skip = (*_l_it)[_chunk_pos].first; skip > 0)
-                        _chunk_pos += skip;
-                    else
-                        break;
-                }
-            }
-
+        public:
             iterator(typename list_t::iterator _b, size_t _l)
                 : _l_it(_b), _limit(_l) {}
 
-        public:
             inline const value_type* operator->() const { return &(_l_it->operator[](_chunk_pos).second); }
 
             inline value_type* operator->() { return &(_l_it->operator[](_chunk_pos).second); }
@@ -74,9 +65,21 @@ namespace Violet
 
             inline value_type&& operator*() && { return _l_it->operator[](_chunk_pos).second; }
 
+            inline stack_pair_t get_pos() const { return { _l_it->get(), _chunk_pos }; }
+
             iterator &operator++() {
                 if (--_limit) {
-                    _iterate();
+                    ++_chunk_pos;
+                    while (true) {
+                        if (_chunk_pos < ChunkSize) {
+                            if (const auto skip = (*_l_it)[_chunk_pos].first; skip > 0)
+                                _chunk_pos += skip;
+                            else
+                                break;
+                        }
+                        else
+                            _chunk_pos = 0;
+                    }
                 }
                 return *this;
             }
@@ -104,13 +107,16 @@ namespace Violet
             friend bool operator!=(const iterator &_it, __end_sentinel _es) {
                 return _it._limit > 0;
             }
-
-            friend class colony;
         };
 
         inline bool empty() const { return _inner_size == 0; }
         inline auto size() const { return _inner_size; }
-        inline void clear() { _data.clear(); _inner_size = 0; }
+
+        inline void clear() {
+            _data.clear();
+            _stack.clear();
+            _inner_size = 0;
+        }
 
         inline typename iterator::__end_sentinel end(void) { return {}; }
 
@@ -118,30 +124,44 @@ namespace Violet
 
         template<class... Args>
         value_type &emplace_at_first_empty(Args&&... args) {
-            for (auto&&it : _data) {
-                for (unsigned int i = 0; i < ChunkSize; ++i)
-                    if (it[i].first > 0) {
-                        if (it[i].first > 1 && i + 1 < ChunkSize) {
-                            it[i + 1].first = it[i].first - 1;
+            if (_stack.empty()) {
+                for (auto&&it : _data) {
+                    for (pos_t i = 0; i < ChunkSize; ++i)
+                        if (it[i].first > 0) {
+                            if (it[i].first > 1 && i + 1 < ChunkSize) {
+                                it[i + 1].first = it[i].first - 1;
+                            }
+                            it[i].first = 0;
+                            ++_inner_size;
+                            return *::new(static_cast<void*>(&(it[i].second))) value_type(std::forward<Args>(args)...);
                         }
-                        it[i].first = 0;
-                        ++_inner_size;
-                        return *::new(static_cast<void*>(&(it[i].second))) value_type(std::forward<Args>(args)...);
-                    }
+                }
+
+                auto ptr = _data.emplace_back(new pair_t[ChunkSize]).get();
+                for (pos_t i = 1; i < ChunkSize; ++i)
+                    ptr[i].first = ChunkSize - i;
+                ++_inner_size;
+                return *::new(static_cast<void*>(&(ptr->second))) value_type(std::forward<Args>(args)...);
             }
-            auto ptr = _data.emplace_back(new pair_t[ChunkSize]).get();
-            //memset(ptr, 0, ChunkSize * sizeof(pair_t));
-            ptr[1].first = ChunkSize;
-            ++_inner_size;
-            return *::new(static_cast<void*>(&(ptr->second))) value_type(std::forward<Args>(args)...);
+            else {
+                const auto top = _stack.top();
+                auto ptr = top.first + top.second;
+                if (ptr->first > 1 && top.second + 1 < ChunkSize) {
+                    ptr[1].first = ptr->first - 1;
+                }
+                ptr->first = 0;
+                _stack.pop();
+                ++_inner_size;
+                return *::new(static_cast<void*>(&(ptr->second))) value_type(std::forward<Args>(args)...);
+            }
         }
 
         void erase(iterator & it) {
-            if (_inner_size > 0) {
-                it.append_next_skip();
-                ++it;
+            it.append_next_skip();
+            _stack.emplace(it.get_pos());
+            ++it;
+            if (_inner_size)
                 --_inner_size;
-            }
         }
     };
 }
